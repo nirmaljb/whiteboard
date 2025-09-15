02bbee352ba036f1a3260a490a570e31e49a1f92
Nirmal Jyoti Biswas <nirmaljyotib@gmail.com>
2025-09-15 19:13:18 +0530

    initial commit

---
+++ apps/websocket/src/index.ts
@@ -0,0 +1,13 @@
+import { Server, WebSocket, WebSocketServer } from 'ws';
+
+const wss = new WebSocketServer({ port: 8080 });
+
+wss.on('connection', function connection(ws: WebSocket) {
+  ws.on('error', console.error);
+
+  ws.on('message', function message(data: any) {
+    console.log('received: %s', data);
+  });
+
+  ws.send('something');
+});
