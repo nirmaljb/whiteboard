e03e6357fa5268a57ac0766b3f331ac61456caa8
Nirmal Jyoti Biswas <nirmaljyotib@gmail.com>
2025-09-17 02:18:49 +0530

    added some basic auth routes

--- apps/http-backend/src/index.ts
+++ apps/http-backend/src/index.ts
@@ -1,10 +1,21 @@
 import express from "express";
 
 const app = express();
+app.use(express());
 
-app.get('/', (req, res) => {
-    res.json({ message: "Hello, World!" });
+app.get('/health', (req, res) => {
+    res.json({ message: "Backend is healthy!" });
 });
+
+app.post('/signup', (req, res) => {
+    const { username, email, password } = req.body;
+    res.json({ success: true, payload: req.body });
+});
+
+app.post('/login', (req, res) => {
+    const { email, password } = req.body;
+    res.json({ success: true, payload: req.body });
+})
 
 app.listen(8000);
 
