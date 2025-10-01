becf6bd9e243fef9b078d725a26fd94ae6f52eaa
Nirmal Jyoti Biswas <nirmaljyotib@gmail.com>
2025-10-02 01:23:12 +0530

    added something for the sake of committing

--- apps/websocket/src/index.ts
+++ apps/websocket/src/index.ts
@@ -3,26 +3,41 @@ import jwt from 'jsonwebtoken';
 
 const wss = new WebSocketServer({ port: 8080 });
 
+function jwtTokenValidation(token: string): string | null {
+    
+    const decoded = jwt.verify(token, 'secret');
+    
+    if(typeof decoded === 'string') {
+        return null;
+    }
+
+    if(!decoded || !decoded.userId) {
+        return null;
+    }
+
+    //@ts-ignore
+    return decoded.sub.user_id;
+} 
+
 wss.on('connection', function connection(ws: WebSocket, request: Request) {
   ws.on('error', console.error);
 
     const url = request.url;
     if(!url) return;
+
     
     const connectionUrl = new URL(url);
     const params = connectionUrl.searchParams; 
     const token = params.get('token') || "";
-    const decoded = jwt.verify(token, 'secret');
-    
-    if(typeof decoded === 'string') {
+
+    const decodedUserId = jwtTokenValidation(token);
+    if(!decodedUserId) {
         ws.close();
         return;
     }
 
-    if(!decoded || !decoded.userId) {
-        ws.close();
-        return;
-    }
+    
+    
 
     ws.on('message', function message(data: any) {
         console.log('received: %s', data);
